dc06191f5b2ea8395fd2f287f5477f98a0f43a87
thezeusexperience-lgtm <thezeusexperience@gmail.com>
2026-03-23 23:51:14 -0400

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'strategy-lab-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(['/']))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).then((fetchResponse) => {
+        if (event.request.url.startsWith(self.location.origin)) {
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, fetchResponse.clone());
+          });
+        }
+        return fetchResponse;
+      });
+    }).catch(() => caches.match('/'))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+});
